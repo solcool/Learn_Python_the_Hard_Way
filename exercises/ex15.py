4a7e8f1418913258f2a634b707d1529a2acb539a
Saul Yang <solcool@126.com>
2017-04-04 17:52:27 +0800

    some exercises about lesson15

--- exercises/ex15.py
+++ exercises/ex15.py
@@ -1,9 +1,23 @@
 from sys import argv
-script, filename = argv
+
+script , filename = argv
+
 txt = open(filename)
-print "Here's your file %r:" % filename
+
+print "Here's your file name : %r" % filename
+
+print "And this is the text in it:"
+
 print txt.read()
+
 print "Type the filename again:"
-file_again = raw_input(">")
-txt_again = open(file_again)
-print txt_again.read()
+
+filename = raw_input(">")
+
+print "And this is the text in the file:"
+
+txt = open(filename)
+
+print txt.read()
+
+txt.close()
